221c1da4ca1e1393280612a4a683424e01ee9ab7
lxb <chris0592@163.com>
2016-02-02 09:44:01 +0800

    Initial Commit

---
+++ CLNetworking/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CLNetworking
+//
+//  Created by Chris on 16/2/2.
+//  Copyright © 2016年 chrislian. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
